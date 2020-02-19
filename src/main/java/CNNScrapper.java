1b6163842cb04b3f9eb3b2010a71baabacf44b55
almasuddin <almas.bangladesh@gmail.com>
2020-02-19 11:07:19 +0600

    Threading Completed

--- src/main/java/CNNScrapper.java
+++ src/main/java/CNNScrapper.java
@@ -12,19 +12,30 @@ import java.util.List;
 
 public class CNNScrapper {
     public static void main(String[] args) throws IOException, FeedException {
+        ImageScrapper img = new ImageScrapper();
+        img.start();
+
         try {
-            //The link of CNN Feed from where we will get the feed entries
-            URL feedUrl = new URL("http://rss.cnn.com/rss/edition.rss");
+            // Execute Main thread every 15 Seconds that will scrap everything from CNN RSS Feed
+            while (true) {
+                try {
+                    //The link of CNN Feed from where we will get the feed entries
+                    URL feedUrl = new URL("http://rss.cnn.com/rss/edition.rss");
 
-            SyndFeedInput input = new SyndFeedInput();
-            SyndFeed feed = input.build(new XmlReader(feedUrl));
+                    SyndFeedInput input = new SyndFeedInput();
+                    SyndFeed feed = input.build(new XmlReader(feedUrl));
 
-            Writer writer = new FileWriter("D:\\a.rss");
-            SyndFeedOutput syndFeedOutput = new SyndFeedOutput();
-            syndFeedOutput.output(feed, writer);
-            writer.close();
-        } catch (Exception ex) {
-            System.out.println("Error: " + ex.getMessage());
+                    Writer writer = new FileWriter("D:\\a.rss");
+                    SyndFeedOutput syndFeedOutput = new SyndFeedOutput();
+                    syndFeedOutput.output(feed, writer);
+                    writer.close();
+                } catch (Exception ex) {
+                    System.out.println("Error: " + ex.getMessage());
+                }
+                Thread.sleep(15 * 1000);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
--- src/main/java/ImageScrapper.java
+++ src/main/java/ImageScrapper.java
@@ -1,32 +1,46 @@
 import java.io.File;
+import java.io.FileWriter;
+import java.io.Writer;
+import java.net.URL;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
+
+import com.sun.syndication.feed.synd.SyndFeed;
+import com.sun.syndication.io.SyndFeedInput;
+import com.sun.syndication.io.SyndFeedOutput;
+import com.sun.syndication.io.XmlReader;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
 import org.w3c.dom.Element;
 
-public class ImageScrapper{
+public class ImageScrapper extends Thread{
+        public void run(){
+            try {
+                // Execute Second thread every 20 Seconds that will scrap Image Reference from local Feed
+                while (true) {
+                    try {
+                        File inputFile = new File("D:\\a.rss");
+                        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+                        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+                        Document doc = dBuilder.parse(inputFile);
+                        doc.getDocumentElement().normalize();
+                        NodeList nList = doc.getElementsByTagName("media:content");
 
-    public static void main(String[] args) {
-
-        try {
-            File inputFile = new File("D:\\a.rss");
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(inputFile);
-            doc.getDocumentElement().normalize();
-            NodeList nList = doc.getElementsByTagName("media:content");
-
-            for (int temp = 0; temp < nList.getLength(); temp++) {
-                Node nNode = nList.item(temp);
-                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-                    Element eElement = (Element) nNode;
-                    System.out.println(eElement.getAttribute("url"));
+                        for (int temp = 0; temp < nList.getLength(); temp++) {
+                            Node nNode = nList.item(temp);
+                            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                                Element eElement = (Element) nNode;
+                                System.out.println(eElement.getAttribute("url"));
+                            }
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    Thread.sleep(20 * 1000);
                 }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
-    }
 }
